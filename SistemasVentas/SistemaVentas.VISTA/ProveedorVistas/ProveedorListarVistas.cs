64c2fd68c1a65cc80e4c64c8efceea9dd211277e
Jhoel Serrudo <serrudocruz17@gmail.com>
2024-03-14 00:35:16 -0400

    Insertar Clases

---
+++ SistemasVentas/SistemaVentas.VISTA/ProveedorVistas/ProveedorListarVistas.cs
@@ -0,0 +1,26 @@
+﻿using SistemasVentas.BSS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVentas.VISTA.ProveedorVistas
+{
+    public partial class ProveedorListarVistas : Form
+    {
+        public ProveedorListarVistas()
+        {
+            InitializeComponent();
+        }
+        ProveedorBss bss = new ProveedorBss();
+        private void ProveedorListarVistas_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = bss.ListarProveedorBss();
+        }
+    }
+}
---
+++ SistemasVentas/SistemaVentas.VISTA/TipoProdVistas/TipoProdListarVistas.cs
@@ -0,0 +1,26 @@
+﻿using SistemasVentas.BSS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVentas.VISTA.TipoProdVistas
+{
+    public partial class TipoProdListarVistas : Form
+    {
+        public TipoProdListarVistas()
+        {
+            InitializeComponent();
+        }
+        TipoProdBss bss = new TipoProdBss();
+        private void TipoProdListarVistas_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = bss.ListarTipoProdBss();
+        }
+    }
+}
